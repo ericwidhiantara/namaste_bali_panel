dc16e59e0bb4d9984838bc9bba17153cc1a7a098
eric <nukenin.konoha@gmail.com>
2024-05-04 15:10:26 +0700

    feature: update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ if sudo docker ps -a --format '{{.Names}}' | grep -q '^namaste_bali_panel$'; the
     sudo docker stop namaste_bali_panel && sudo docker rm namaste_bali_panel &&
     sudo npm install && sudo npm run build &&
     sudo docker build -t namaste_bali_panel . &&
-    sudo docker run --name namaste_bali_panel -p 3001:3001 -d namaste_bali_panel
+    sudo docker run --name namaste_bali_panel -env-file .env -p 3001:3001 -d namaste_bali_panel
 else
    sudo npm install && sudo npm run build &&
        sudo docker build -t namaste_bali_panel . &&
-       sudo docker run --name namaste_bali_panel -p 3001:3001 -d namaste_bali_panel
+       sudo docker run --name namaste_bali_panel -env-file .env -p 3001:3001 -d namaste_bali_panel
 fi
