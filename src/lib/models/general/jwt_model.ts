b57a943eda62f5db4bee62dd3a043b337a82244d
eric <nukenin.konoha@gmail.com>
2024-05-07 14:39:53 +0700

    feature: add jwt payload extend to override the default jwt payload

--- src/lib/models/general/jwt_model.ts
+++ src/lib/models/general/jwt_model.ts
@@ -1,13 +1,20 @@
 import { UserDataModel } from '$lib/models/users/user_model';
+import type { JwtPayload } from 'jwt-decode';
+
+export interface ExtendedJwt extends JwtPayload {
+	exp: number;
+	sub: string;
+	user: UserDataModel;
+}
 
 export class JWTModel {
 	exp: number;
-	sub: number;
+	sub: string;
 	user: UserDataModel;
 
 	constructor(data: Partial<JWTModel> = {}) {
 		this.exp = data.exp || 0;
-		this.sub = data.sub || 0;
+		this.sub = data.sub || '';
 		this.user = data.user || new UserDataModel();
 	}
 }
